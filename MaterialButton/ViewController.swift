54ecc54dfea8b628ed32a9d3e3f4e7845e064462
fromkk <ueoka@fromkk.info>
2014-12-29 11:01:04 +0900

    Initial Commit

---
+++ MaterialButton/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MaterialButton
+//
+//  Created by Kazuya Ueoka on 2014/12/29.
+//  Copyright (c) 2014年 fromKK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
